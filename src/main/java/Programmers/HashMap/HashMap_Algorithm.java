3814a733f856d6537e6d8c2b94efcd2d96d7f072
leechaeeun <chaenny0516@gmail.com>
2022-08-02 21:40:39 +0900

    [add] 프로그래머스 스택/큐 같은 숫자는 싫어

--- src/main/java/Programmers/HashMap/HashMap_Algorithm.java
+++ src/main/java/Programmers/HashMap/HashMap_Algorithm.java
@@ -2,4 +2,8 @@ package Programmers.HashMap;
 
 public class HashMap_Algorithm {
 
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ src/main/java/Programmers/StackQueue/Lessons_2906.java
@@ -0,0 +1,46 @@
+package Programmers.StackQueue;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * 같은 숫자는 싫어
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java
+ * 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+ *
+ * arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+ * arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+ * 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 제한사항
+ * 배열 arr의 크기 : 1,000,000 이하의 자연수
+ * 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+ */
+public class Lessons_2906 {
+
+    public int[] solution(int []arr) {
+        ArrayList<Integer> tempAnswer = new ArrayList<>();
+        tempAnswer.add(arr[0]);
+        for(int i=1; i< arr.length; i++){
+            int recentValue = tempAnswer.get(tempAnswer.size()-1); // 인덱스의 마지막 값
+            if(recentValue != arr[i]){
+                tempAnswer.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[tempAnswer.size()];
+        for(int i=0; i<tempAnswer.size(); i++) {
+            answer[i] = tempAnswer.get(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Lessons_2906 lessons_2906 = new Lessons_2906();
+        int[] testcase1 = {1, 1, 3, 3, 0, 1, 1};
+        int[] answer1 = {1, 3, 0, 1};
+        int[] result1 = lessons_2906.solution(testcase1);
+        assert result1.equals(answer1);
+
+    }
+}
